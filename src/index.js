998058dbb05e229358e686691c1157caa651fca5
seylioneldev <seymourlionel97@gmail.com>
2024-01-17 21:12:41 +0100

    Mise en place user-page

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
-import React from 'react';
-import App from './App';
+import React from "react";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Products } from './pages/Products/Products';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { AboutUs } from './pages/AboutUs/AboutUs';
-import { ContactUs } from './pages/ContactUs/ContactUs';
-import { Cart } from './pages/Cart/Cart';
-import { Login } from './pages/Login/Login';
-import { Register } from './pages/Register/Register';
-import {HomePage} from './pages/HomePage/HomePage';
+import { Products } from "./pages/Products/Products";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { AboutUs } from "./pages/AboutUs/AboutUs";
+import { ContactUs } from "./pages/ContactUs/ContactUs";
+import { Cart } from "./pages/Cart/Cart";
+import { Login } from "./pages/Login/Login";
+import { Register } from "./pages/Register/Register";
+import { HomePage } from "./pages/HomePage/HomePage";
 import s from "./style.module.css";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className={`${s.container}`}>
-  <BrowserRouter>
-  <Routes> 
-    <Route path="/" element={<App />}>
-      <Route path="/homepage" element={<HomePage />}/>  
-      <Route path="/products" element={<Products />}/>  
-      <Route path="/about-us" element={<AboutUs />}/>  
-      <Route path="/contact-us" element={<ContactUs />}/>  
-      <Route path="/cart" element={<Cart />}/>  
-      <Route path="/login" element={<Login />}/>  
-      <Route path="/register" element={<Register />}/>  
-    </Route>
-    
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </div>
-
 );
-
-
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,7 +2,37 @@ import s from './style.module.css'
 
 export function Register({}){
     return(
-        <>
-        <h1>Register</h1>
-        </>
+        <div className={`${s.container}`}>
+            <form className={`${s.form}`}>
+                <p className={`${s.title}`}>Register </p>
+                <p className={`${s.message}`}>Signup now and get full access to our app. </p>
+                    <div className={`${s.flex}`}>
+                    <label>
+                        <input required="" placeholder="" type="text" className={`${s.input}`}/>
+                        <span>Firstname</span>
+                    </label>
+
+                    <label>
+                        <input required="" placeholder="" type="text" className={`${s.input}`}/>
+                        <span>Lastname</span>
+                    </label>
+                </div>  
+                        
+                <label>
+                    <input required="" placeholder="" type="email" className={`${s.input}`}/>
+                    <span>Email</span>
+                </label> 
+                    
+                <label>
+                    <input required="" placeholder="" type="password" className={`${s.input}`}/>
+                    <span>Password</span>
+                </label>
+                <label>
+                    <input required="" placeholder="" type="password" className={`${s.input}`}/>
+                    <span>Confirm password</span>
+                </label>
+                <button className={`${s.submit}`}>Submit</button>
+                <p className={`${s.signin}`}>Already have an acount ? <a href="#">Signin</a> </p>
+            </form>
+        </div>
         )}
